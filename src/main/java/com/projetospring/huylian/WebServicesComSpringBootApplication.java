b635d7bcf430723908ed1577a6c8d11f0bfae94d
huylianbarros <huylian2020@gmail.com>
2022-03-23 20:24:19 -0300

    first commit

---
+++ src/main/java/com/projetospring/huylian/WebServicesComSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.projetospring.huylian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServicesComSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServicesComSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetospring/huylian/WebServicesComSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetospring.huylian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServicesComSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
