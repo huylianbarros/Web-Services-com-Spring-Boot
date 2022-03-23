82b37b88b868e36748a498acdba962f8bd66412f
huylianbarros <huylian2020@gmail.com>
2022-03-23 20:55:31 -0300

    User entity and resource

--- src/main/java/com/projetospring/huylian/WebServicesComSpringBootApplication.java
+++ src/main/java/com/projetospring/huylian/WebServicesComSpringBootApplication.java
@@ -8,6 +8,7 @@ public class WebServicesComSpringBootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebServicesComSpringBootApplication.class, args);
+		
 	}
 
 }
